b0ebe1726796bccb1739ede1e5f94d687e7e7cca
MuhammadAbdullah95 <ma2404374@gmail.com>
2025-02-18 22:43:19 +0500

    All files are ready with code skeleton

---
+++ src/research_agent/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import os
+from dotenv import load_dotenv
+from research_agent.agents import ResearchAgent
+from research_agent.data_loader import DataLoader
+
+
+load_dotenv()
+
+print("ok")
